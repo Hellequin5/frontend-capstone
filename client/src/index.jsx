b399e127ec307358af24e66039ad7db28d45c1b6
Patrick Lorden <pjlorden@gmail.com>
2021-10-28 17:08:59 -0500

    Implemented Basic Axios request for initial State

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,41 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-
+import API_KEY from './config.js';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product_id: []
-    }
+      product_id: null
+    };
+    this.singleItemRequest = this.singleItemRequest.bind(this);
+  }
+
+  componentDidMount() {
+    this.singleItemRequest(38322);
+  }
+
+  singleItemRequest (id) {
+    var config = {
+      method:'get',
+      url:`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/products/${id}`,
+      headers: {
+        'Authorization': API_KEY
+      }
+    };
+
+    axios(config)
+      .then((resolve) => {
+        console.log(resolve.data.id);
+        this.setState({
+          product_id: resolve.data.id
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+
   }
 
   render () {
