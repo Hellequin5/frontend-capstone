2f326f3492be0e4230913ee3af0bf379d793e973
Patrick Lorden <pjlorden@gmail.com>
2021-10-28 16:49:17 -0500

    Refactor App to Class Component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function() {
-  return(
-    <div>This is a test!</div>
-  )
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product_id: []
+    }
+  }
+
+  render () {
+    return (
+      <div> This is a Test</div>
+    )
+  }
 }
+// const App = function() {
+//   return(
+//     <div>This is a test!</div>
+//   )
+// }
 
 // "this is a test commit"
 ReactDOM.render(<App />, document.getElementById('app'));
