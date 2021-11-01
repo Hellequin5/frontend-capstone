f3c0828bb823008dbad0e44cba858e87e16f064f
Jeff DeSouza <jeffdesouzadev@gmail.com>
2021-11-01 12:47:05 -0500

    First crack at a reasonable component structure

--- client/src/questions/index.jsx
+++ client/src/questions/index.jsx
@@ -12,3 +12,28 @@ const Questions = (props) => {
 }
 
 module.exports = Questions;
+
+/*
+<QuestionsSearch>
+<QuestionsBody>
+-- <Question#1 Wrapper>
+--## <Question#1 Question>
+--## <Question#1 Answer>
+--## <Question#1 Footer>
+-- <Question#2 Wrapper>
+--## <Question#2 Question>
+--##++ <Question#2Question Text>
+--## <Question#2 Answers>
+--##++ <Question#2Answer#1 Text>
+--##++ <Question#2Answer#1 Images>
+--##++ <Question#2Answer#2 Text>
+--##++ <Question#2Answer#2 Images>
+--## <Question#2 Answers Footer>
+--## <Question#2 Footer>
+--##++ <Question#2 New Answer>
+<QuestionsFooter>
+-- <New Question>
+
+Revision1: Questions may not have Images - only Answers
+Revision2: Many Answers to One Question
+*/
