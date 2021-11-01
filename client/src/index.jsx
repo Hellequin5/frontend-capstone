84988e91c6b0cb4af3be360993b462cbb2aa0849
Patrick Lorden <pjlorden@gmail.com>
2021-11-01 13:59:39 -0500

    Cleaned up src/index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -33,6 +33,7 @@ function App() {
   }
 
   /* Use Effects second paramenter makes sure that it only runs when the app is mounted like component did mount */
+  
   useEffect(() => {
     singleItemRequest();
   }, [])
@@ -47,56 +48,5 @@ function App() {
     </div>
   )
 }
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       product_id: null
-//     };
-//     this.singleItemRequest = this.singleItemRequest.bind(this);
-//     // this.getStartingPid = this.getStartingPid.bind(this);
-//   }
-
-//   componentDidMount() {
-//     this.singleItemRequest();
-//   }
-
-//   singleItemRequest (id) {
-
-//     var config = {}
-//     if (id) {
-//       config = {
-//         method:'get',
-//         url:`http://localhost:10038/singleItemRequest`,
-//         params: {product_id: id}
-//       };
-//     } else {
-//       config = {
-//         method: 'get',
-//         url: 'http://localhost:10038/starting_product_id'
-//       }
-//     }
-//     axios(config)
-//       .then((resolveProductInfo) => {
-//         this.setState({
-//           product_id: resolveProductInfo.data.product_id
-//         })
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//       })
-//   }
-
-//   render () {
-//     return (
-//       <div>
-//         <Overview product_id={this.state.product_id} />
-//         <Questions product_id={this.state.product_id} />
-//         <Reviews product_id={this.state.product_id} />
-//       </div>
-//     )
-//   }
-// }
 
 ReactDOM.render(<App />, document.getElementById('app'));
