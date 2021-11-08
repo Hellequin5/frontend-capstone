c1396b7cbe4a7a8680464d0e6368043d0aa25bea
Patrick Lorden <pjlorden@gmail.com>
2021-11-07 19:01:11 -0600

    Added react-bootstrap functionality and updated webpack

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
@@ -34,7 +37,7 @@ function App() {
   }
 
   /* Use Effects second paramenter makes sure that it only runs when the app is mounted like component did mount */
-  
+
   useEffect(() => {
     singleItemRequest();
   }, [])
