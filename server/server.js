b7fecc43cfb0a8b44b736643fd28034a9c1c8d63
Patrick Lorden <pjlorden@gmail.com>
2021-11-11 14:38:14 -0600

    removed routes from server.js that should be in reviews/index.js

--- server/server.js
+++ server/server.js
@@ -53,66 +53,6 @@ app.get('/singleItemRequest', (req, res) => {
     })
 });
 
-app.get('/productReviews', (req, res) => {
-  var config = {
-    method: 'get',
-    url:`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/reviews/`,
-    headers: {
-      'Authorization': API_KEY
-    },
-    params: {
-      page: req.query.page,
-      count: req.query.count,
-      sort: req.query.sort,
-      product_id: req.query.product_id
-    }
-  }
-    axios(config)
-      .then((response) => {
-        res.status(200).send(response.data)
-      })
-      .catch((err) => {
-        res.status(404).send('Unable to retrieve Product data')
-      })
-})
-
-app.get('/productMetaData', (req, res) => {
-  var id = req.query.product_id;
-  var config = {
-    method: 'get',
-    url:`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/reviews/meta/?product_id=${id}`,
-    headers: {
-      'Authorization': API_KEY
-    }
-  };
-  axios(config)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      res.status(500).send('Unable to retrieve product Meta Data');
-    })
-})
-
-app.put('/reviews', (req, res) => {
-  var id = req.query.review_id;
-  var type = req.query.type;
-  var config = {
-    method: 'put',
-    url:`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/reviews/${id}/${type}`,
-    headers: {
-      'Authorization': API_KEY
-    }
-  };
-  axios(config)
-    .then((response) => {
-      res.status(204).send('request accepted')
-    })
-    .catch((err) => {
-      res.status(500).send('request failed')
-    })
-})
-
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`)
 })
