3ed05d901cf94d030db27d1a50ac1bf2955be169
Jeff DeSouza <jeffdesouzadev@gmail.com>
2021-11-12 19:48:06 -0600

    refactor

--- server/questions/index.js
+++ server/questions/index.js
@@ -9,25 +9,18 @@ module.exports = function(app) {
 
   app.get('/get_item_questions', (req, res) => {
     //console.log(req);
-    var pid = req.query.pid
-    var page = req.query.page
-    if (page) {
-      page = '&page='+page;
-    }
-    else {
-      page = '';
-    }
-    var count = req.query.count
-    if (count) {
-      count = '&count='+count;
-    }
-    else {
-      count = '';
-    }
-    console.log('pid is', pid);
+    var pid = req.query.pid;
+    var page = req.query.page;
+    (page) ? page = '&page='+page : page = '';
+    var count = req.query.count;
+    (count) ? count = '&count='+count : count = '';
+
+    var endpoint =  `https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/qa/questions?product_id=${pid}${page}${count}`;
+
+    console.log('get endpoint is', endpoint);
     var config = {
       method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/qa/questions?product_id=${pid}${page}${count}`,
+      url: endpoint,
       headers: {
         'Authorization': API_KEY
       }
@@ -107,14 +100,11 @@ module.exports = function(app) {
 
 
   app.post('/add_answer', (req, res) => {
-    //console.log(req);
-    //var qid = req.query.qid.toString();
     var qid = parseInt(req.query.qid);
     var my_body = req.query.body.toString();
     var my_name = req.query.name.toString();
     var my_email = req.query.email.toString();
-    console.log('request query is', req.query)
-    //console.log('fields are ', product_id, body, name, email);
+
     var request_body = {
       body : my_body,
       name : my_name,
@@ -132,12 +122,14 @@ module.exports = function(app) {
       .then((addAnswerResponse) => {
         var answers = addAnswerResponse.data;
         res.status(addAnswerResponse.status).send(answers)
-        console.log('add answer response was "', answers , '" status:', addAnswerResponse.status);
       })
       .catch((error) => {
         console.error(error);
-        res.status(500).send(`There was a problem inserting a question.`)
+        res.status(500).send(`There was a problem inserting that answer.`)
       })
   });
-  console.log('questions finished loading');
+
+
+
+  console.log('questions routes finished loading');
 }
